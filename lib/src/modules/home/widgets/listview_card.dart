c36d283887db60b0000c2c73b82b03695d4617c6
RegisSalesRA <regismsales@gmail.com>
2022-10-04 10:44:07 -0300

    componentizando widget List

---
+++ lib/src/modules/home/widgets/listview_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../../../model/model.dart';
+import 'widgets.dart';
+
+class ListViewCard extends StatelessWidget {
+  const ListViewCard({
+    Key? key,
+    required this.data,
+    required this.searchString,
+  }) : super(key: key);
+
+  final List<Result>? data;
+  final String searchString;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: data!.length,
+        itemBuilder: (_, index) {
+          return data![index].name.toLowerCase().contains(searchString)
+              ? InkWell(
+                  onTap: () => {
+                    FocusScope.of(context).unfocus(),
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => DetailWidget(
+                              image: data![index].image,
+                              tag: data![index].id.toString(),
+                              name: data![index].name,
+                              location: data![index].location.name,
+                              origin: data![index].origin.name,
+                              gender: data![index].gender!,
+                              species: data![index].species!,
+                              status: data![index].status!,
+                              episode: data![index].episode,
+                            )))
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Center(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Hero(
+                              tag: data![index].id.toString(),
+                              child: Image.network(
+                                data![index].image,
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            child: Text(
+                              data![index].name,
+                              style: const TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              : Container();
+        });
+  }
+}
--- lib/src/modules/home/widgets/widgets.dart
+++ lib/src/modules/home/widgets/widgets.dart
@@ -3,3 +3,4 @@ export './details.dart';
 export './header.dart';
 export './search_bar.dart';
 export './grid_card.dart';
+export './listview_card.dart';
