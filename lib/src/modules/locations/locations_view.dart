f14308be4a424772d9e3c0e3247f95656d51a528
RegisSalesRA <regismsales@gmail.com>
2022-10-06 22:22:20 -0300

    start location Page

--- lib/src/modules/locations/locations_view.dart
+++ lib/src/modules/locations/locations_view.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
- 
+import 'package:rick_and_morty/src/modules/locations/widgets/earth_location_screen.dart';
+
 class LocationScreen extends StatelessWidget {
   const LocationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Locations List")),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+            child: Column(
+          children: [
+            InkWell(
+              child: const Text("Earth"),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const EarthLocationScreen()));
+              },
+            ),
+            const Text("Cidatel")
+          ],
+        )),
+      ),
     );
   }
 }
---
+++ lib/src/modules/locations/widgets/earth_location_screen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+import '../../../../config/config.dart';
+
+class EarthLocationScreen extends StatefulWidget {
+  const EarthLocationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EarthLocationScreen> createState() => _EarthLocationScreenState();
+}
+
+class _EarthLocationScreenState extends State<EarthLocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SafeArea(
+            child: Stack(
+      children: [
+        SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+                height: size.height * 0.40,
+                child: Hero(
+                    tag: 'widget.tag',
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(50)),
+                      child: Image.asset(
+                        'assets/images/earth.png',
+                        fit: BoxFit.fill,
+                      ),
+                    ))),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Earth",
+                style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: 150,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    border: Border.all(
+                        color: Colors.grey.shade300,
+                        width: 2,
+                        style: BorderStyle.solid)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: const [
+                        Text(
+                          "Type: ",
+                          style: TextStyle(
+                              overflow: TextOverflow.ellipsis,
+                              color: Colors.pink,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: AnimatedFadedText(
+                            direction: 1,
+                            child: Text(
+                              'Planet',
+                              maxLines: 1,
+                              style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.lightGreen,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: const [
+                        Text(
+                          "First Episode: ",
+                          style: TextStyle(
+                              overflow: TextOverflow.ellipsis,
+                              color: Colors.pink,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: AnimatedFadedText(
+                            direction: 1,
+                            child: Text(
+                              'Pilot',
+                              maxLines: 1,
+                              style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.lightGreen,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text(
+                          "Dimension: ",
+                          style: TextStyle(
+                              overflow: TextOverflow.ellipsis,
+                              color: Colors.pink,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: AnimatedFadedText(
+                            direction: 1,
+                            child: Text(
+                              'Dimension C-137',
+                              maxLines: 1,
+                              style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                  color: Colors.lightGreen,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Residents: ",
+                style: TextStyle(
+                    color: Colors.pink,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ]),
+        ),
+        Positioned(
+            top: 20,
+            left: 20,
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.pink,
+              ),
+            ))
+      ],
+    )));
+  }
+}
